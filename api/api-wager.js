5ddbc370cabffa91194396fba691e2690321cb55
vanjaandrejic <vanjaandrejic5@gmail.com>
2024-07-19 17:45:08 +0200

    wager test 1

---
+++ api/api-wager.js
@@ -0,0 +1,44 @@
+import fetch from "node-fetch";
+
+export default async (req, res) => {
+  const url = "https://ords.365.rs/ords/pwin/ic_user_ords/GET_USER_STAT";
+
+  // Dobijanje IP adrese iz zahteva
+  const requestIpAddress =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(`Request received from IP address: ${requestIpAddress}`);
+
+  try {
+    const apiResponse = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        p_username: "marcco",
+        p_password: "newfrontback",
+        p_from_date: "01.07.2024",
+        p_to_date: "19.07.2024",
+        p_online_user: "bakaprase",
+      },
+    });
+
+    if (!apiResponse.ok) {
+      throw new Error(`API call failed with status: ${apiResponse.status}`);
+    }
+
+    // Logovanje IP adrese servera koji je odgovorio
+    const responseIpAddress =
+      apiResponse.headers.get("x-forwarded-for") || "N/A";
+    console.log(`Response received from IP address: ${responseIpAddress}`);
+
+    const data = await apiResponse.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("API call error:", error.message);
+    res.status(500).json({
+      error: "Failed to fetch data from the external API",
+      message: error.message,
+    });
+  }
+};
--- src/pages/results-page.tsx
+++ src/pages/results-page.tsx
@@ -1,6 +1,33 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 const ResultPage: FC = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/api-wager", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // body: JSON.stringify({
+
+          // }),
+        });
+
+        if (!response.ok) {
+          const errorText = await response.text();
+          throw new Error(`Server error: ${errorText}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div>ResultPage</div>;
 };
 
